2dbe32da6640c2b4e872780947c5198df155de7a
JuHyun226 <59912535+JuHyun226@users.noreply.github.com>
2020-01-16 16:33:55 +0900

    Update ch03-Quiz1_cin.cpp

--- ch03-Quiz1_cin.cpp
+++ ch03-Quiz1_cin.cpp
@@ -1,21 +1,37 @@
-
 // ch03-Quiz_cin.cpp
 
 #include <iostream>
 
 int main() {
 
-	//이름,나이,키,성별을 입력받아 출력하는 코드 작성
-	//-성별은 'm' or 'f'로 처리하고,m이면 "남성", f이면 "여성"으로 출력
+	// 이름, 나이, 키, 성별을 입력받아서 출력하는 코드를 작성하세요
+	// - 성별은 'm' or 'f' 로 처리하고, m이면 "남성" f 이면 "여성"으로 출력하세요
+	char name[20];
+	std::cout << "이름 입력 > ";
+	std::cin >> name;
 
-	char name[] = "정주현";
-	int age = 22;
-	int height = 168;
-	char gender[] = 'f';
+	int age;
+	std::cout << "나이 입력  > ";
+	std::cin >> age;
 
+	double height;
+	std::cout << "키   입력 > ";
+	std::cin >> height;
+
+	char gender;
+	while (1) {
+		std::cout << "성별 입력 > ";
+		std::cin >> gender;
+		if (gender == 'm' || gender == 'f')
+			break;
+	}
+
+	std::cout << "--- 입 력 정 보 ---" << std::endl;
 	std::cout << "이름 : " << name << std::endl;
 	std::cout << "나이 : " << age << " 세" << std::endl;
-	std::cout << "키 : " << height << " cm" << std::endl;
-	std::cout << "성별 : " << gender << std::endl;
-
+	std::cout << "키   : " << height << " cm" << std::endl;
+	std::cout << "성별 : ";
+	if (gender == 'm') std::cout << "남성";
+	else std::cout << "여성";
+	std::cout << std::endl;
 }
