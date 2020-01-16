010744c1c9ae1e3eb7c468bca73795ca83c89da4
JuHyun226 <59912535+JuHyun226@users.noreply.github.com>
2020-01-16 17:05:14 +0900

    Create ch02_cin.cpp

---
+++ ch02_cin.cpp
@@ -0,0 +1,65 @@
+// ch02_cin.cpp
+
+#include <iostream>
+
+/*
+cin
+- cin 뒤에 오른쪽 쉬프트 연산자(>>)를 사용하여 입력받은 데이터를 변수에 저장합니다
+*/
+
+int main() {
+
+	//// 정수
+	//int v1;
+	//std::cout << "숫자 입력 > ";
+	//std::cin >> v1;
+	//std::cout << "v1 : " << v1 << std::endl;
+	//std::cout << std::endl;
+
+	//// 실수
+	//double v2;
+	//std::cout << "실수 입력 > ";
+	//std::cin >> v2;
+	//std::cout << "v2 : " << v2 << std::endl;
+	//std::cout << std::endl;
+
+	//// 문자
+	//char v3;
+	//std::cout << "문자 입력 > ";
+	//std::cin >> v3;
+	//std::cout << "v3 : " << v3 << std::endl;
+	//std::cout << std::endl;
+
+
+	//int last;
+	//std::cout << "마지막 수 입력 > ";
+	//std::cin >> last;
+
+	//int sum = 0;
+	//for (int i = 1; i <= last; i++) {
+	//	sum += i;
+	//}
+	//std::cout << "1 ~ " << last << " 까지의 합 : " << sum << std::endl;
+
+
+	int n1, n2;
+	std::cout << "숫자 2개 입력 > ";
+	std::cin >> n1 >> n2;
+	std::cout << "n1 : " << n1 << ", n2 : " << n2 << std::endl;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
