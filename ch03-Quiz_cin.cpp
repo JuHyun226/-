bfffafb9f09879c6eeb468bf516c00fb246bb1ca
JuHyun226 <59912535+JuHyun226@users.noreply.github.com>
2020-01-16 16:05:54 +0900

    Create ch03-Quiz_cin.cpp
    이거왜...실행시키면 
    
    '"D:\정주현\정주현20200116\Debug\정주현2020-01-16.exe"'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는
    배치 파일이 아닙니다.
    계속하려면 아무 키나 누르십시오 . . .
    
    가 뜨는지 의문...

---
+++ ch03-Quiz_cin.cpp
@@ -0,0 +1,34 @@
+
+// ch03-Quiz_cin.cpp
+
+#include <iostream>
+
+int main() {
+
+	//이름,나이,키,성별을 입력받아 출력하는 코드 작성
+	//-성별은 'm' or 'f'로 처리하고,m이면 "남성", f이면 "여성"으로 출력
+
+	char name[] = "정주현";
+	int age = 22;
+	int height = 168;
+	char gender[] = 'f';
+
+	std::cout << "이름 : " << name << std::endl;
+	std::cout << "나이 : " << age << " 세" << std::endl;
+	std::cout << "키 : " << height << " cm" << std::endl;
+	std::cout << "성별 : " << gender << std::endl;
+
+
+	//정수 5개를 입력받아서 입력받은 수의 합을 구하는 코드를 작성
+	//- 마이너스(-)값은 처리 불가
+
+
+
+	// 세과목의 점수를 입력받앙서 총점, 평균, 학점을 구하는 코드작성
+	// - int : 점수, 총점
+	//	 double: 평균
+	//   char: 학점
+	// - 평균을 사용해서 학점을 구하세요
+	// A : 100~90, B : 89~90, C : 79~70, D : 69~60, F : 59~60
+
+}
