92307bd24c12dc4b655615aabf164854a2d1b042
JuHyun226 <59912535+JuHyun226@users.noreply.github.com>
2020-01-29 18:10:59 +0900

    Create ch15-Quiz_class-access.cpp

---
+++ ch15-Quiz_class-access.cpp
@@ -0,0 +1,22 @@
+/*
+	ch15-Quiz_class-access.cpp
+*/
+
+#include <iostream>	
+#include <string>
+
+using namespace std;
+
+// Computer class를 정의하고 main()에서 테스트하세요
+// - 필드 : 모델명, cpu, ram, 가격
+//   기능 : 각각의 필드값 설정, 수정, 사용 가능
+//          > 모델명 : 20자 이내
+//            ram    : 단위 G(기가)
+//            가격   : -(마이너스) 설정 불가, 단위 만원
+//          정보 확인 가능
+
+int main() {
+
+
+
+}
