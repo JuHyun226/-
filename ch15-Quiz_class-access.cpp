c9c77995e922ad1d20fb2e4008b4c4ec01471601
JuHyun226 <59912535+JuHyun226@users.noreply.github.com>
2020-01-30 16:58:21 +0900

    Update ch15-Quiz_class-access.cpp

--- ch15-Quiz_class-access.cpp
+++ ch15-Quiz_class-access.cpp
@@ -15,8 +15,61 @@ using namespace std;
 //            가격   : -(마이너스) 설정 불가, 단위 만원
 //          정보 확인 가능
 
+class Computer {
+private:
+	string model;
+	string cpu;
+	int ram;
+	int price;
+
+public:
+
+	string getModel() { return model; }
+	void setModel(string _model) {
+		if (_model.length() < 21) {
+			model = _model;
+		}
+	}
+
+	string getCpu() { return cpu; }
+	void setCpu(string _cpu) {
+		cpu = _cpu;
+	}
+
+	int getRam() { return ram; }
+	void setRam(int _ram) {
+		if (_ram >= 0) {
+			ram = _ram;
+		}
+	}
+
+	int getPrice() { return price; }
+	void setPrice(int _price) {
+		if (_price >= 0) {
+			price = _price;
+		}
+	}
+
+	void info() {
+		cout << "----- 컴퓨터 사양 -----" << endl;
+		cout << "모델명 : " << model << endl;
+		cout << "CPU    : " << cpu << endl;
+		cout << "RAM    : " << ram << " G" << endl;
+		cout << "가격   : " << price << endl;
+	}
+
+};
+
+
 int main() {
 
-
+	Computer proA;
+	proA.setModel("1234ksdajdfkl;ja;klsdf");
+	proA.setModel("2020 gram");
+	proA.setCpu("i7");
+	proA.setRam(8);
+	proA.setPrice(122);
+	proA.info();
 
 }
+
